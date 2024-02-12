63eca29bf84b8cf5d934587434c31192548a2dc6
oumaima maghraoui <maghraouioumaima0@gmail.com>
2024-02-12 15:22:05 +0100

    this is first commit

---
+++ PiDevBackend/src/Controller/RendezVousController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RendezVousController extends AbstractController
+{
+    #[Route('/rendezvous', name: 'app_rendez_vous')]
+    public function index(): Response
+    {
+        return $this->render('rendezvous.html.twig', [
+            'controller_name' => 'RendezVousController',
+        ]);
+    }
+}
