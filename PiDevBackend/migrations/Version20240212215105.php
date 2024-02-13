6cd5320c9f1ae6163d227d9e4dcbc6b9ffb67f6a
oumaima maghraoui <maghraouioumaima0@gmail.com>
2024-02-13 20:17:39 +0100

    this is second commit template fiche works with db and rvd still in progress

---
+++ PiDevBackend/migrations/Version20240212215105.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240212215105 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE fichepatient (id INT AUTO_INCREMENT NOT NULL, weight INT DEFAULT NULL, muscle_mass INT DEFAULT NULL, height INT DEFAULT NULL, allergies VARCHAR(255) DEFAULT NULL, illnesses VARCHAR(255) DEFAULT NULL, breakfast VARCHAR(255) DEFAULT NULL, midday VARCHAR(255) DEFAULT NULL, dinner VARCHAR(255) DEFAULT NULL, snacks VARCHAR(255) DEFAULT NULL, calories INT DEFAULT NULL, other LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE fichepatient');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
